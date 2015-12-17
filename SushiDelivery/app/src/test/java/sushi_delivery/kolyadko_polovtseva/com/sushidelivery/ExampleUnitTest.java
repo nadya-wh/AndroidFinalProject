734ae0423c68fa9abba60bed87d33271a686993e
nadya-wh <nadezhdapolovtseva@gmail.com>
2015-12-17 15:32:18 +0300

    Added main and login activities.

---
+++ SushiDelivery/app/src/test/java/sushi_delivery/kolyadko_polovtseva/com/sushidelivery/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package sushi_delivery.kolyadko_polovtseva.com.sushidelivery;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
